464f45d8ff43f1dce6e869b2fcc56736e72427ea
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2026-08-04 14:22:19 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let result = true;
+        if (s.trim() === "") {
+            return true;
+        }
+        const pattern = /[a-z0-9]/g;
+        const str = s.trim().toLowerCase().match(pattern);
+        if(!str){
+            return true;
+        }
+        for (let i = 0; i < str.length; i++) {
+            const char = str[i];
+            const revChar = str[str.length - (i + 1)];
+            if (char !== revChar) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
